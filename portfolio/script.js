07f15eafa43b302383684ceb7c598db1f463c22b
eolouiszz <ilouiszk@gmail.com>
2025-04-22 04:26:17 -0300

    atualização basica

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,6 +2,7 @@
 function link1() {
     window.location.href = 'https://github.com/eolouiszz/';
 }
+
 function link2() {
-    window.location.href = 'https://www.youtube.com/@louiszika/';
+    window.location.href = 'https://www.instagram.com/sudo.https.louis/'
 }
